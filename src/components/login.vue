c759146dd70a229136aab0b8b4d533e03f36dd10
十只橘猫九只胖 <1083874345@qq.com>
2019-08-14 10:48:16 +0800

    路由设置及登录布局完成

---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="basebox">
+    <div class="login"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+
+  .basebox {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login {
+      width: 580px;
+      height: 436px;
+      background-color: #fff;
+      border-radius: 5px;
+    }
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/base.less'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import login from './components/login.vue'
+import index from './components/index.vue'
+const routes = [
+  {path:'/login',component:login},
+  {path:'/index',component:index},
+]
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
